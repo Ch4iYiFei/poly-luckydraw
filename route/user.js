3eaba20e339434ed67d4113785074cb00725d7ef
Ch4iYiFei <1679376406@qq.com>
2019-06-18 19:15:59 +0800

    string

--- route/user.js
+++ route/user.js
@@ -1,16 +1,21 @@
-var router = require("express").Router();
+var express = require("express");
+var router = express.Router();
 var fs = require("fs");
 
 
 router.post("/login", (req, res) => {
     //   console.log(req.method+req.statusCode);
-    //  console.log(req);
+    console.log("/usr/login");
     console.log(req.body);
-    console.log(req.method);
-    console.log(req.protocol);
+    // console.log(req.method);
+    // console.log(req.protocol);
     var appid = "wx55bd9c881859ddb5";
-    var appsecret = fs.readFileSync("AppSecret");
+    var appsecret = fs.readFileSync("AppSecret").toString;
     console.log(appsecret);
+
+    if(req.body.code){
+        console.log(req.body.code);
+    }
     var url = "https://api.weixin.qq.com/sns/jscode2session?appid="+appid+"&secret="+appsecret+"&js_code="+req.body+"&grant_type=authorization_code";
     res.send("get login message");
 });
