821bb748d755cf075b06a1657ca5257730867f2c
Ch4iYiFei <1679376406@qq.com>
2019-06-18 16:36:38 +0800

    appsecret3

--- route/user.js
+++ route/user.js
@@ -2,9 +2,9 @@ var router = require("express").Router();
 var fs = require("fs");
 
 router.get("/login", (req, res) => {
-    //res.send("get login message");
-    console.log(req.method+req.statusCode);
-    console.log(req);
+    res.send("get login message");
+ //   console.log(req.method+req.statusCode);
+  //  console.log(req);
     console.log(req.body);
     var appid = "wx55bd9c881859ddb5";
     var appsecret = fs.readFileSync("AppSecret");
