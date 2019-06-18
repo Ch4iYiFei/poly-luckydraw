3df9044272a23fd05901f672ec6909e7bc274a0a
Ch4iYifei <cyfg00gle@gmail.com>
2019-06-17 22:52:27 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+var fs = require('fs');
+
+var key = fs.readFileSync('privatekey.pem');
+var cert = fs.readFileSync('certificate.crt');
+
+var https = require("https");
+
+https.createServer({
+	key:key,
+	cert:cert
+},app).listen(8086,function() {
+	console.log("run on 8086");
+});
+
