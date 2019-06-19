57e2d76c6ae815ad6c7840a25c69a7f12aa9e05f
Ch4iYiFei <1679376406@qq.com>
2019-06-19 09:28:02 +0800

    api6

--- route/user.js
+++ route/user.js
@@ -2,27 +2,36 @@ var express = require("express");
 var router = express.Router();
 var fs = require("fs");
 var https = require("https");
+var qs = require("querystring");
+var request = require("request");
 
 router.post("/login", (req, res) => {
     //   console.log(req.method+req.statusCode);
     console.log("/usr/login");
-    console.log(req.body);
-    // console.log(req.method);
-    // console.log(req.protocol);
+    console.log("body:"+req.body);
     var appid = "wx55bd9c881859ddb5";
     // var appsecret = fs.readFileSync("AppSecret").toString();
     var appsecret = require("../AppSecret");
-    console.log(appsecret);
+    // console.log(appsecret);
 
     if(req.body.code){
         console.log(req.body.code);
-        console.log("https://api.weixin.qq.com/sns/jscode2session?appid="+appid+"&secret="+appsecret+"&js_code="+req.body.code+"&grant_type=authorization_code");
-        var options = new URL("https://api.weixin.qq.com/sns/jscode2session?appid="+appid+"&secret="+appsecret+"&js_code="+req.body.code+"&grant_type=authorization_code");
-        
-        https.request(options, (res) => {
+        var param = {
+            appid: appid,
+            secret: appsecret,
+            js_code: req.body.code,
+            grant_type: "authorization_code"
+        };
+        var options = {
+            method = "get",
+            url = "https://api.weixin.qq.com/sns/jscode2session?" + qs.stringify(param)
+        };
+        console.log(options.url);
+
+        var req = https.request(options,(res)=>{
             console.log(res.statusCode);
-            console.log(res);
-        });
+            console.log(res.body);
+        })
     }
     res.send("get login message");
 });
