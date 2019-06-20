df7d8b57677d5f76996fc50c932ba6219a614041
Ch4iYiFei <1679376406@qq.com>
2019-06-20 11:31:57 +0800

    final1

--- route/user.js
+++ route/user.js
@@ -5,7 +5,8 @@ var https = require("https");
 var qs = require("querystring");
 var request = require("request");
 var MongoClient = require('mongodb').MongoClient;
-const db_url = 'mongodb://127.0.0.1:27017';
+// const db_url = 'mongodb://103.209.102.252:27017/lucky';
+const db_url = 'mongodb://127.0.0.1:27017/lucky';
 
 router.post("/login", (req, resback) => {
     //   console.log(req.method+req.statusCode);
@@ -30,58 +31,6 @@ router.post("/login", (req, resback) => {
         };
         console.log(options.url);
 
-        // var req = https.request(options,(res)=>{
-        //     console.log(res.statusCode);
-        //     console.log(res.body);
-        // })
-
-        // new Promise((resolve,reject)=>{
-        //     console.log("start new promise");
-        //     request(options, (err, res, body) => {
-        //         if(err)
-        //         {
-        //             console.log("into reject");
-        //             reject(err);
-        //         }else {
-        //             console.log("into resolve");
-        //             resolve(res);
-        //         }
-        //     })
-        // }).then((res)=>{
-        //     console.log(res.statusCode);
-        //     console.log(body);
-        //     MongoClient.connect(db_url,{ useNewUrlParser: true },(db_err,db) => {
-        //         if(db_err) throw db_err;
-        //         var dbase = db.db("lucky");
-        //         console.log("db connected");
-
-        //         var col = dbase.collection("user");
-        //         col.find({id:response.openid}).toArray((find_err,find_result)=>{
-        //             if(find_err)  throw find_err;
-        //             if(find_result.length == 0){
-        //                 console.log("之前未登录的用户");
-        //                 //可增加更多的insert项
-        //                 col.insertOne({ id:response.openid },(insert_err,insert_result)=>{
-        //                     if(insert_err) throw insert_err;
-        //                     console.log("插入用户成功");
-        //                 })
-                        
-        //             }else{
-        //                 console.log("用户存在，直接登录");
-        //                 console.log(find_result);
-                        
-        //             }
-        //             //不管几次登录都返回一样的信息
-        //             //s生成jwt
-        //             resback.send({error:null});
-        //             db.close();
-        //         })
-        //     })
-        // }).catch((err)=>{
-        //     console.log("error occured when accesing weixinserver");
-        //     resback.send({error:"访问微信api出现问题"});
-        // })
-
         request(options, (err, res, body) => {
             console.log(res.body+"\n"+err)
             if (body) {
@@ -90,6 +39,7 @@ router.post("/login", (req, resback) => {
                 var response = JSON.parse(body);
                 console.log(response.openid);
                 //拿到了微信个人id
+                //此处可能内部有出现新的进程，导致新的进程进入else执行，所以写一个返回中断下面的逻辑？？？
                 return MongoClient.connect(db_url,{ useNewUrlParser: true },(db_err,db) => {
                     if(db_err) throw db_err;
                     var dbase = db.db("lucky");
