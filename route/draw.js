8637d92157849d0cac2fa05ce981c39c669e2aef
Ch4iYiFei <1679376406@qq.com>
2019-06-20 16:35:35 +0800

    photo

--- route/draw.js
+++ route/draw.js
@@ -12,18 +12,31 @@ var uuidv1 = require("uuid/v1");
 var multer = require("multer");
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './upload')
+        cb(null, './upload');
     },
     filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + uuidv1())
+        cb(null, file.fieldname + '-' + uuidv1());
     }
 });
 var upload = multer({storage: storage});
 
 router.post("/publish", upload.single("draw"), (req, resback) => {
     console.log("/draw/publish");
+    var file = req.file;
     console.log(req.file);
+    console.log(req.body);
     
+    MongoClient.connect(db_url,{ useNewUrlParser: true },(db_err,db) => {
+        if(db_err) throw db_err;
+        var dbase = db.db("lucky");
+        console.log("db connected");
+
+        var col = dbase.collection("draw");
+        console.log("文件类型",file.mimetype);
+        console.log('文件保存路径：%s', file.path);
+        //var object = {};
+        //col.insertOne()
+    });
 });
 
 
