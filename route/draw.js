ecde01d0f51e2d96e261dbc34b0e6514a3cfba64
Ch4iYiFei <1679376406@qq.com>
2019-06-21 16:47:28 +0800

    all

--- route/draw.js
+++ route/draw.js
@@ -89,6 +89,31 @@ router.post("/fetch/publish", (req,resback)=>{
 
 });
 
+router.post("/fetch/all", (req,resback)=>{
+    console.log("/draw/fetch/all");
+    console.log(req.body);
+    var token = req.body.jwt;
+    var publisher = jwt.decode(token,secret).iss;
+    //已经拥有的抽奖信息数量
+    var skipnum = req.body.num;
+    var limitnum = 10;
+    console.log("发布者",publisher);
 
+    MongoClient.connect(db_url,{ useNewUrlParser: true },(db_err,db) => {
+        if(db_err) throw db_err;
+        var dbase = db.db("lucky");
+        console.log("db connected");
+
+        var col = dbase.collection("draw");
+        //isPublic对所有用户来说是选择条件
+        col.find({isPublic: true}).skip(skipnum).limit(limitnum).toArray((find_err,find_result)=>{
+            if(find_err)  throw find_err;
+            console.log(find_result);
+            resback.send(find_result);
+            db.close();
+        });
+    });
+
+});
 
 module.exports = router;
