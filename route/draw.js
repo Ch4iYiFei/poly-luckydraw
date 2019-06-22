9eebf2d77fe990aa7219c77cc69850743b2054ac
Ch4iYiFei <1679376406@qq.com>
2019-06-22 11:34:40 +0800

    update

--- route/draw.js
+++ route/draw.js
@@ -138,15 +138,23 @@ router.post("/join", (req,resback)=>{
         
         var col = dbase.collection("joiner");
 
-        var object = {draw_id: req.body.draw_id, joiner: joiner};
-        col.updateOne
-        // col.insertOne(object, (insert_err,insert_result)=>{
+        //var object = {draw_id: req.body.draw_id, joiner: joiner};
+        
+        // col.insertOne({draw_id: req.body.draw_id}, (insert_err,insert_result)=>{
         //     if(insert_err) throw insert_err;
         //     console.log("用户参与成功");
         //     //...........
         //     resback.send({error: null});
         //     db.close();
         // });
+
+        col.updateOne({draw_id: req.body.draw_id},{$addToSet:{joiner: joiner}},(insert_err,insert_result)=>{
+            if(insert_err) throw insert_err;
+            console.log("用户参与成功");
+            //...........
+            resback.send({error: null});
+            db.close();
+        });
     });
 });
 
