47483e2a671b378dadab3fa5e89f58edd3acbfe9
Ch4iYiFei <1679376406@qq.com>
2019-06-25 14:52:46 +0800

    task

--- route/task.js
+++ route/task.js
@@ -21,6 +21,10 @@ module.exports = {
             var time_arr = element.time.split(":");
             var date = new Date(date_arr[0],date_arr[1],date_arr[2],time_arr[0],time_arr[1]);
             console.log(date.toLocaleTimeString());
+            schedule.scheduleJob(date,()=>{
+                console.log("大概可能就会执行");
+                this.messagesend(element.draw_id);
+            })
         });
     },
 //可能会出现删除抽奖后的问题
